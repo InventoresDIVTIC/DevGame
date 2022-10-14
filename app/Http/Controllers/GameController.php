3ac1e89683e01c13f6b1f2aa5a201c93108b0fc5
davotorres <david.torrest98@gmail.com>
2022-10-14 15:24:33 -0500

    funcion userData Publica

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -10,13 +10,13 @@ class GameController extends Controller
     public function __construct()
     {
         //Protege la pagina de perfil, es necesario inciar session antes
-        $this->middleware('auth')->except(['index', 'store']);
+        $this->middleware('auth')->except(['userData']);
     }
     
     //lo lleva a la vista del juego
     public function index(){
-        // $user = User::find(auth()->user()->id);
-        $user = User::find(1);
+        $user = User::find(auth()->user()->id);
+        //$user = User::find(1);
        
         return view('game.index',[
             'user' => $user
@@ -25,8 +25,8 @@ class GameController extends Controller
 
     //toma los datos del usuario para ser mandados al juego
     public function userData(){
-        $user = User::find(auth()->user()->id);
-        //$user = User::find(1);
+      //$user = User::find(auth()->user()->id);
+        $user = User::find(1);
         return $user;
     }
 
