7b08b62a92dc873ae137edde9a760dbc0b666776
davotorres <david.torrest98@gmail.com>
2022-10-14 16:33:11 -0500

    Se actualiza juego, y se pone a prueba API en el servidor

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -10,7 +10,7 @@ class GameController extends Controller
     public function __construct()
     {
         //Protege la pagina de perfil, es necesario inciar session antes
-        $this->middleware('auth')->except(['userData']);
+        $this->middleware('auth');
     }
     
     //lo lleva a la vista del juego
@@ -25,8 +25,8 @@ class GameController extends Controller
 
     //toma los datos del usuario para ser mandados al juego
     public function userData(){
-      //$user = User::find(auth()->user()->id);
-        $user = User::find(1);
+        $user = User::find(auth()->user()->id);
+        //$user = User::find(1);
         return $user;
     }
 
