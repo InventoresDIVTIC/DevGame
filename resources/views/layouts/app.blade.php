c14c2bed8d71fa58f3713768f171984f2f53c41c
davotorres <david.torrest98@gmail.com>
2022-10-17 15:08:02 -0500

    liveware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <link href="{{asset('css/app.css')}}" rel="stylesheet">
         @stack('script')
         <script src="{{asset('js/app.js')}}" defer></script>
+        
         @livewireStyles
 </head>
     <body class="bg-gray-100">
@@ -62,6 +63,7 @@
         <footer class="text-center p-5 font-bold text-gray-600 uppercase">
             DevsGame- Todos los derechos reservados {{ now()->year}}
         </footer>
+        
         @livewireScripts
     </body>
 </html>
