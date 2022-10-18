885ccff9f507b8eb1209c53808d59e51643623f5
davotorres <david.torrest98@gmail.com>
2022-10-18 15:46:55 -0500

    Se añade () a livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
         <link href="{{asset('css/app.css')}}" rel="stylesheet">
         @stack('script')
         <script src="{{asset('js/app.js')}}" defer></script>
-        @livewireStyles
-        @livewireScripts
+        @livewireStyles()
+        @livewireScripts()
    </head> 
     <body class="bg-gray-100">
         <header class="p-5 border-b bg-white shadow">
